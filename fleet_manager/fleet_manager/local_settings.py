0523b079b706fe077b71b3acebcc88fc9e8781cf
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-06 13:57:38 -0400

    Created DB connection; Created local_settings; migrated

---
+++ fleet_manager/fleet_manager/local_settings.py
@@ -0,0 +1,17 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-g864dv_!%1-7jo+=clx%vt(0l%$l(ce$^wb9%6u39(*r$g3_21'
+
+#Local Database settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'fleet_manager_database',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}
--- fleet_manager/fleet_manager/settings.py
+++ fleet_manager/fleet_manager/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_q3z$ajrayc$2^f5-4k6x_wlx@6n!z*)*p4-q5lx3u3u-%%cnp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,17 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'fleet_manager.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -123,3 +110,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from fleet_manager.local_settings import *
+except ImportError:
+    pass
