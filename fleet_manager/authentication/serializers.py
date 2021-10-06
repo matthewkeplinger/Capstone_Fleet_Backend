c7208aed3c5e706187cbebc7f87e5f98b1028350
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-06 14:33:26 -0400

    Ability to create and log in user verified in Postman; look into phone number later

--- fleet_manager/authentication/serializers.py
+++ fleet_manager/authentication/serializers.py
@@ -11,15 +11,14 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username','password','email','first_name','last_name', 'phone')
+        fields = ('username','password','email','first_name','last_name')
 
     def create(self, validated_data):
         user = User.objects.create(
             username = validated_data['username'],
             email = validated_data['email'],
             first_name = validated_data['first_name'],
-            last_name = validated_data['last_name'],
-            phone= validated_data['phone']
+            last_name = validated_data['last_name']
         )
         user.set_password(validated_data['password'])
         user.save()
