1c59e072e6f53504b1591503ddead9fddc2b7d3a
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-06 14:17:07 -0400

    Added authentication app; created RegistrationSerializer, Regiser View

--- fleet_manager/fleet_manager/settings.py
+++ fleet_manager/fleet_manager/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication.apps.AuthenticationConfig'
 ]
 
 MIDDLEWARE = [
@@ -111,6 +113,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
 try:
     from fleet_manager.local_settings import *
 except ImportError:
